f8d2c73005c589b153094d1eb44ea4dadec7e3b2
夜刀神十香 <tohka@kamijuu.local>
2023-05-22 07:33:05 +0800

    added interpolate function

--- test.py
+++ test.py
@@ -28,34 +28,6 @@ raw_data = np.fromfile(
             dtype='uint16', 
             sep='').reshape(RESOLUTION)
 
-# r_indices = (
-#     (0, 2), (2, 0)
-# )
-        
-# g_indices = (
-#     (0, 1), (0, 3), 
-#     (1, 0), (1, 2), 
-#     (2, 1), (2, 3), 
-#     (3, 0), (3, 2)
-# )
-
-# b_indices = (
-#     (0, 2), (0, 2)
-# )
-
-# q_indices = (
-#     (1, 1), (1, 3), 
-#     (3, 1), (3, 3)
-# )
-
-# forward = (
-#     (1, 3), (1, 3)
-# )
-
-# backward = (
-#     (1, 3), (3, 1)
-# )
-
 r_indices = (
     # rows, cols
     (0, 2), (2, 0)
@@ -81,8 +53,6 @@ forward = (
 backward = (
     (1, 3), (3, 1)
 )
-
-
 
 KERNEL = {
     "R": r_indices,
@@ -172,6 +142,10 @@ def interpolate_q(source):
     source[b_row, b_col] = b
 
 
+def interpolate(source):
+    interpolate_q(source)
+    interpolate_red(source)
+
 def save_to_txt(name, obj):
     output_path = os.path.join(OUTPUT_DIR, name)
     np.savetxt(output_path, obj, fmt='%d')
@@ -183,12 +157,10 @@ start = time.time()
 # print(interpolate_red(raw_data))
 # print(interpolate_q(raw_data))
 # print(raw_data)
-# interpolate_red(raw_data)
-interpolate_q(raw_data)
 # print(filter(raw_data, 'Q'))
 # print(raw_data)
-
-save_to_txt('fuck_you_q.txt', filter(raw_data, 'Q'))
+interpolate(raw_data)
+save_to_txt('fuck_you_interpolation.txt', raw_data)
 
 # print(get_base_table('f').astype(int))
 # print(get_base_table('b').astype(int))
@@ -200,93 +172,3 @@ save_to_txt('fuck_you_q.txt', filter(raw_data, 'Q'))
 # save_to_txt('fuck_me.txt', interpolate_red(raw_data))
 end = time.time()
 print(end - start)
-
-# # reds = filter_by_color(raw_data, "R")
-# # greens = filter_by_color(raw_data, "G")
-# # blues = filter_by_color(raw_data, "B")
-# # infrareds = filter_by_color(raw_data, "Q")
-# # # print(infrareds)
-
-# # def filter_rows_by_color(source, color):
-# #     return np.asarray(
-# #         [source[row] if color[row].any() else [0] * RESOLUTION[1] 
-# #          for row in range(RESOLUTION[0])]
-# #         )
-
-# # def filter_columns_by_color(source, color):
-# #     color_transpose = color.transpose()
-# #     source_transpose = source.transpose()
-# #     return np.asarray(
-# #         [source_transpose[column] if color_transpose[column].any() else [0] * RESOLUTION[0] 
-# #          for column in range(RESOLUTION[1])]
-# #         ).transpose()
-
-# def get_color_index(source, color):
-#     return np.column_stack(np.where(truth_table(get_base_table(color))))
-
-# def build(source, indices):
-#     template = np.zeros(source.shape, dtype="uint16")
-#     for index in indices:
-#         template[index[0]][index[1]] = source[index[0]][index[1]]
-#     return template
-    
-# def filter_by_color(source, color):
-#     return build(source, get_color_index(source, color))
-
-# # red_indices = get_color_index(raw_data, "R")
-# # print(red_indices)
-# # print(build(raw_data, red_indices))
-
-# # print(filter_by_color(raw_data, "R"))
-# # print(filter_by_color(raw_data, "G"))
-# # print(filter_by_color(raw_data, "B"))
-# # print(filter_by_color(raw_data, "Q"))
-# # mod_green = filter_rows_by_color(greens, reds)
-# # print(reds)
-# # print(greens)
-# # print(mod_green)
-
-
-# def interpolate_point(source, index):
-#     c = 0
-
-#     t = 0
-#     l = 0
-#     d = 0
-#     r = 0
-
-#     row, col = index
-#     t_index = row - 2
-#     l_index = col - 2
-#     d_index = row + 2
-#     r_index = col + 2
-
-#     if t_index > -1:
-#         t = source[t_index][col]
-#         c += 1
-#     if l_index > -1:
-#         l = source[row][l_index]
-#         c += 1
-#     if d_index < source.shape[0]:
-#         d = source[d_index][col]
-#         c += 1
-#     if r_index < source.shape[1]:
-#         r = source[row][r_index]
-#         c += 1
-
-#     source[row][col] = math.floor((t + d + l + r) / c)
-
-# def interpolate_red(source):
-#     color_indices = get_color_index(source, "R")
-#     source_copy = source.copy()
-#     for index in color_indices:
-#         interpolate_point(source_copy, index)
-#     return source_copy
-
-# 
-# start = time.time()
-# print(filter_by_color(raw_data, "R"))
-# # filter_by_color(interpolate_red(raw_data), "R")
-# # save_to_txt("lmao.txt", filter_by_color(interpolate_red(raw_data), "R"))
-# end = time.time()
-# print(end - start)
